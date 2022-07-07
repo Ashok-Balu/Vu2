e15bca591fefebebdcd1f1bb0fbe478454f6385b
Ashok-Balu <107237056+Ashok-Balu@users.noreply.github.com>
2022-07-07 17:45:37 +0530

    Create ChildComp.vue

---
+++ Sample/components/ChildComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Training registration status: {{ msgReceived }}</p>
+    <button @click="resetVal">Reset</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ChildComp",
+    props: ["msgReceived"],
+    methods: {
+    resetVal: function () {
+      //this.msgReceived = "Cancelled";
+      this.$emit('msgGotReset', "Cancelled");
+    },
+  },
+  };
+</script>
+<style scoped>
+ h3 {
+  color: orange;
+  font-family: arial;
+}
+</style>

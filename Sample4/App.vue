aa47f99606ccd1bc490350729f73dd47aaffedc1
Ashok-Balu <107237056+Ashok-Balu@users.noreply.github.com>
2022-07-07 18:16:50 +0530

    Add files via upload

---
+++ Sample4/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :style="{ marginLeft: '2%' }">
+    <h2>UI & Markup Technology</h2>
+    <br />
+    <router-link to="/">Home </router-link> |
+    <router-link to="/about">About us </router-link> |
+    <router-link to="/training">Training </router-link> |
+    <router-link to="/contact">Contact us </router-link> 
+    
+    <hr />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

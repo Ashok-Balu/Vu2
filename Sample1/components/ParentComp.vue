4034853ad98dca045a342651c0eb2c2fbb57fd17
Ashok-Balu <107237056+Ashok-Balu@users.noreply.github.com>
2022-07-07 17:50:03 +0530

    Add files via upload

---
+++ Sample1/components/ParentComp.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        
+    <p>Rendered from Parent component!</p>
+    <hr />
+    <ValidateCmp
+      :skill="skills"
+      :empObj="empDetails"
+      :id="empId"
+      :dc="location"
+    />
+    <hr />
+ </div>
+</template>
+
+<script>
+import ValidateCmp from './ValidateCmp.vue';
+    export default {
+    components: { 
+        ValidateCmp ,
+     },
+     data() {
+        return {
+        empId: 681592,
+        empDetails: { name: "Samual", unit: "Technical" },
+        skills: ["React"],
+        location: "Mysore",
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Sample1/components/ValidateCmp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <p>
+      I'm from ValidateCmp - received below props from Parent and validated<br /><br />
+      {{ skill }}<br />
+      {{ empObj}} <br />
+      {{ id }} <br />
+      {{ dc }}
+    </p>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: "ValidateCmp",
+    props: {
+        skill:{
+          type:Array,
+          required:true,
+          //default: ["vue"],
+        } ,
+         empObj:{
+          type:Object,
+          required:true,
+          // default:
+          //  { name: "Ashok", 
+          //    unit: "Technical" },
+         } , 
+         id: Number,
+         dc: {
+           type:String,
+            required:true,
+            default:'hii', 
+         }
+    }
+    }
+</script>
+
+<style scoped>
+h3 {
+  color: orange;
+  font-family: arial;
+}
+</style>

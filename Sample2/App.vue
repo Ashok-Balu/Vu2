c2b7a08596ad6ef85a3fbb399d36fe495085fec4
Ashok-Balu <107237056+Ashok-Balu@users.noreply.github.com>
2022-07-07 18:08:22 +0530

    Add files via upload

---
+++ Sample2/App.vue
@@ -0,0 +1,37 @@
+<template>
+   <div :style="{ marginLeft: '2%' }">
+    <h2>Validating form controls</h2>
+    <hr />
+   <!-- <ValidateForm /> -->
+    <AssesmentValidation/>
+    <HttpSample />
+    
+  </div>
+</template>
+
+<script>
+
+
+ 
+
+
+export default {
+ 
+  name: 'App',
+  components: {
+  
+},
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
